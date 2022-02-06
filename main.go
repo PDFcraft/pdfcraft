a00f89ecc0c23d0b71d3ef5fee3cc62395531160
chobobdev <chobobdev@naver.com>
2022-02-06 16:55:11 +0900

    FEAT: upload file to merge

--- main.go
+++ main.go
@@ -2,25 +2,50 @@ package main
 
 import (
 	"net/http"
+	"path/filepath"
 	"pdfcraft/utils"
 
 	"github.com/gin-gonic/gin"
 )
 
+func handleFunc(c *gin.Context) {
+	// file path in server
+	c.File("out.pdf")
+}
+
 func main() {
-	inFiles := []string{"in1.pdf", "in2.pdf"}
-	utils.MergePdfFile(inFiles)
-	r := gin.Default()
+	router := gin.Default()
+	// Set a lower memory limit for multipart forms (default is 32 MiB)
+	router.MaxMultipartMemory = 8 << 20 // 8 MiB
+	router.LoadHTMLGlob("templates/*")
 
-	r.LoadHTMLGlob("templates/*")
-
-	r.GET("/", func(c *gin.Context) {
-		// OK 이면 index.html파일에 JSON데이터를 넘겨서 보여줌
-		c.HTML(http.StatusOK, "index.html", gin.H{
-			"title": "Home Page",
-		},
-		)
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", gin.H{})
 	})
 
-	r.Run()
+	inFiles := []string{}
+
+	router.POST("/merge", func(c *gin.Context) {
+		form, err := c.MultipartForm()
+		if err != nil {
+			c.String(http.StatusBadRequest, "get form err: %s", err.Error())
+			return
+		}
+		files := form.File["files"]
+		for _, file := range files {
+			filename := filepath.Base(file.Filename)
+			inFiles = append(inFiles, filename)
+			if err := c.SaveUploadedFile(file, filename); err != nil {
+				c.String(http.StatusBadRequest, "upload file err: %s", err.Error())
+				return
+			}
+		}
+		utils.MergePdfFile(inFiles)
+		c.HTML(http.StatusOK, "merge.html", gin.H{})
+
+	})
+
+	router.GET("/download", handleFunc)
+
+	router.Run(":8080")
 }
