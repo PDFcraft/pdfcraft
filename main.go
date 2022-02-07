4d1b36ba69d7a47d2b20c841a9669f8e9ee2d3b1
chobobdev <chobobdev@naver.com>
2022-02-08 00:29:22 +0900

    FEAT: naming convention for processed file

--- main.go
+++ main.go
@@ -24,11 +24,14 @@ func saveFileHandler(c *gin.Context) {
 		return
 	}
 	recvFiles := make(map[int]string)
+	originName := make(map[string]string)
 	for i, file := range files {
 		extension := filepath.Ext(file.Filename)
+		originFileName := filepath.Base(file.Filename)
 		newFileName := uuid.New().String() + extension
 		// Linking uuid with sent order with dict
 		recvFiles[i] = newFileName
+		originName[newFileName] = originFileName
 		if err := c.SaveUploadedFile(file, "./temp/"+newFileName); err != nil {
 			c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
 				"message": "Unable to save the file",
@@ -37,14 +40,18 @@ func saveFileHandler(c *gin.Context) {
 		}
 
 	}
-
+	// var name = filepath.Ext(originName[recvFiles[0]])
+	var mergedFileName = originName[recvFiles[0]][0:len(originName[recvFiles[0]])-4] + "-merged" + ".pdf"
+	// var name = TrimRight(originName[recvFiles[0]], ".pdf")
+	utils.MergePdfFile(recvFiles, mergedFileName)
 	// File saved successfully. Return proper result
 	c.JSON(http.StatusOK, gin.H{
-		"message": "Your file has been successfully uploaded.",
-		"files":   recvFiles,
+		"message":        "Your file has been successfully uploaded.",
+		"files":          recvFiles,
+		"originName":     originName,
+		"mergedFileName": mergedFileName,
 	})
 
-	utils.MergePdfFile(recvFiles)
 }
 
 func main() {
--- utils/mergepdf.go
+++ utils/mergepdf.go
@@ -4,10 +4,10 @@ import (
 	pdfcpu "github.com/pdfcpu/pdfcpu/pkg/api"
 )
 
-func MergePdfFile(recvFiles map[int]string) {
+func MergePdfFile(recvFiles map[int]string, mergedFileName string) {
 	inFiles := []string{}
 	for _, filename := range recvFiles {
 		inFiles = append(inFiles, "./temp/"+filename)
 	}
-	pdfcpu.MergeCreateFile(inFiles, "./output/merged.pdf", nil)
+	pdfcpu.MergeCreateFile(inFiles, "./output/"+mergedFileName, nil)
 }
