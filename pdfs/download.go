937a9712fbf4ad0c7cc1b1ff1d2d6f2a56edf726
chobobdev <chobobdev@naver.com>
2022-02-11 02:51:40 +0900

    Fix: CORS fixed

--- pdfs/download.go
+++ pdfs/download.go
@@ -6,6 +6,12 @@ import (
 )
 
 func Download(c *gin.Context) {
+	c.Header("Access-Control-Allow-Headers", "Content-Type, Authorization, Origin")
+	c.Header("Access-Control-Allow-Credentials", "true")
+	c.Header("Access-Control-Allow-Origin", "http://localhost:3000")
+	c.Header("Access-Control-Allow-Methods", "GET, DELETE, POST")
+	c.Next()
+
 	fileid := c.Param("fileid")
 	originFileName := db.GetFileNamePair(fileid + ".pdf")
 	c.FileAttachment("./output/"+fileid+".pdf", originFileName)
--- pdfs/mergepdf.go
+++ pdfs/mergepdf.go
@@ -11,6 +11,12 @@ import (
 )
 
 func MergeHandler(c *gin.Context) {
+	c.Header("Access-Control-Allow-Headers", "Content-Type, Authorization, Origin")
+	c.Header("Access-Control-Allow-Credentials", "true")
+	c.Header("Access-Control-Allow-Origin", "http://localhost:3000")
+	c.Header("Access-Control-Allow-Methods", "GET, DELETE, POST")
+	c.Next()
+	
 	form, err := c.MultipartForm()
 	if err != nil {
 		c.String(http.StatusBadRequest, "get form err: %s", err.Error())
